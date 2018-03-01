f086d0958bae199c6679a8702f4ca0ef8ac7316d
Tony Delmond <tonydelmond@hotmail.com>
2018-03-01 10:12:16 -0800

    create analysis.py script

---
+++ analysis.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#saved as analysis.py
+#%% Imports the library pandas with abbreviated call "pd"
+import pandas as pd
+
+#%% Imports the data set using the pandas library command to read csv file
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
+
+#%% Descriptive statistics for the data set imported
+print(dat.describe())
